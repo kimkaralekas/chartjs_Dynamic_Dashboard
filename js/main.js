ceefe02354e4cde2f4b9498e05c26e6822ac63a9
Kim Karalekas <kimkaralekas@gmail.com>
2015-04-01 09:01:25 -0500

    add third line widget

--- js/main.js
+++ js/main.js
@@ -67,3 +67,26 @@ var data = [
 var canvas = document.getElementById("hours");
 var ctx = canvas.getContext("2d");
 new Chart(ctx).Doughnut(data);
+
+// Line graph multi data setvar data = {
+    labels : ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
+    datasets : [
+        {
+            fillColor : "rgba(99,123,133,0.4)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            data : [65,54,30,81,56,55,40]
+        },
+        {
+            fillColor : "rgba(219,186,52,0.4)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            data : [20,60,42,58,31,21,50]
+        },
+    ]
+}
+var canvas = document.getElementById("shipments");
+var ctx = canvas.getContext("2d");
+new Chart(ctx).Line(data, options);
