683aa3ca6d247c258c5dd63fc43ff3c4c561f088
Kim Karalekas <kimkaralekas@gmail.com>
2015-04-01 08:52:30 -0500

    add code to create doughnut graph

--- js/main.js
+++ js/main.js
@@ -43,3 +43,27 @@ function redraw(animation){
     // ....
 }
 size(); // this kicks off the first drawing
+
+// Create doughnut graph
+var data = [
+    {
+        value: 20,
+        color:"#637b85"
+    },
+    {
+        value : 30,
+        color : "#2c9c69"
+    },
+    {
+        value : 40,
+        color : "#dbba34"
+    },
+    {
+        value : 10,
+        color : "#c62f29"
+    }
+
+];
+var canvas = document.getElementById("hours");
+var ctx = canvas.getContext("2d");
+new Chart(ctx).Doughnut(data);
